1b15f1483f88fa4a532f0920d8128a825e8884d7
Natalia Munina <nataly.munina@gmail.com>
2020-04-28 17:20:02 +0200

    scenario outline added try catch implemented

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -9,10 +9,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Random;
+
 public class StepDefinitions {
+    Random rnd = new Random();
     WebDriver driver;
     String message;
     String actualResult;
+    //String[] testCase;
+    String testCase;
 
     @Given("user is on {string}")
     public void user_is_on(String string) throws InterruptedException {
@@ -26,64 +31,95 @@ public class StepDefinitions {
         driver.findElement(By.id("cookie-continue-btn")).click();
     }
 
+    @And("has test case {string}")
+    public void hasTestCase(String arg0) {
+        testCase = arg0;
+    }
+
     @And("has entered username {string}")
     public void hasEnteredUsername(String username) {
-        driver.findElement(By.name("username")).sendKeys(username);
+
+        //generate unique username for the "happy case"
+        if (testCase.equals("TC1")) {
+            username = rnd.nextInt(100) + username;
+        }
+        try {
+            driver.findElement(By.name("username")).sendKeys(username);
+        } catch (Exception e) {
+            System.out.println("Could not fill in Username");
+        }
     }
 
     @And("has entered email {string}")
     public void hasEnteredEmail(String email) {
-        driver.findElement(By.id("email")).sendKeys(email);
+        //generate unique username for the "happy case"
+        if (testCase.equals("TC1")) {
+            email = rnd.nextInt(100) + email;
+        }
+
+        try {
+            driver.findElement(By.id("email")).sendKeys(email);
+        } catch (Exception e) {
+            System.out.println("Could not fill in email");
+        }
     }
 
     @And("has entered password {string}")
     public void hasEnteredPassword(String password) {
-        driver.findElement(By.cssSelector("form > div:nth-of-type(3) > div > input")).sendKeys(password);
+        try {
+            driver.findElement(By.cssSelector("form > div:nth-of-type(3) > div > input")).sendKeys(password);
+        } catch (Exception e) {
+            System.out.println("Could not fill in email");
+        }
     }
 
     @And("has entered confirmed password {string}")
-    public void hasEnteredConfirmedPassword(String arg0) throws InterruptedException {
-        driver.findElement(By.cssSelector("input[name='password-confirm']")).sendKeys(arg0);
+    public void hasEnteredConfirmedPassword(String passwordConfirmed) throws InterruptedException {
+        try {
+            driver.findElement(By.cssSelector("input[name='password-confirm']")).sendKeys(passwordConfirmed);
+        } catch (Exception e) {
+            System.out.println("Could not fill in passwordConfirmed");
+        }
         Thread.sleep( 2000);
     }
 
     @When("he clicks to registrate")
     public void heClicksToRegistrate() throws InterruptedException {
-        // When submit form
+        // submit form
+        try {
         driver.findElement(By.id("register")).click();
+        } catch (Exception e) {
+            System.out.println("Could not click on \"Registration\" button");
+        }
         Thread.sleep( 2000);
     }
 
     @Then("he gets {string}")
     public void he_gets(String expectedResult) {
-        //String expectedResult = string;
-        String[] testCase = expectedResult.split(":");
-        switch (testCase[0]) {
-            case "TC1": //#Skapa användare​
-                message = driver.getCurrentUrl();
-                actualResult = "TC1:" + message;
-                break;
+        // Assert results according to the test case
+        try {
+            switch (testCase) {
+                case "TC1": //#Skapa användare​
+                    actualResult = driver.getCurrentUrl();
+                    break;
 
-            case "TC2": //#Skapa användare – långt användarnamn (mer än 16 tecken)​
-                message = driver.findElement(By.id("username-notify")).getText();
-                actualResult = "TC2:" + message;
-                break;
+                case "TC2": //#Skapa användare – långt användarnamn (mer än 16 tecken)​
+                    actualResult = driver.findElement(By.id("username-notify")).getText();
+                    break;
 
-            case "TC3": //#Skapa användare – lösenord och bekräfta lösenord skiljer sig​
-                message = driver.findElement(By.id("password-confirm-notify")).getText();
-                actualResult = "TC3:" + message;
-                break;
+                case "TC3": //#Skapa användare – lösenord och bekräfta lösenord skiljer sig​
+                    actualResult = driver.findElement(By.id("password-confirm-notify")).getText();
+                    break;
 
-            case "TC4": //#Skapa användare – E-postadress är redan använd​
-                message = driver.findElement(By.id("email-notify")).getText();
-                actualResult = "TC4:" + message;
-                break;
+                case "TC4": //#Skapa användare – E-postadress är redan använd​
+                    actualResult = driver.findElement(By.id("email-notify")).getText();
+                    break;
+            }
+        } catch (Exception e) {
+            System.out.println(testCase + "did not pass");
         }
 
         Assert.assertEquals(expectedResult, actualResult);
-        System.out.println("then" + expectedResult);
-
         driver.close();
-
     }
 }
--- src/test/resources/hellocucumber/newuser.feature
+++ src/test/resources/hellocucumber/newuser.feature
@@ -1,9 +1,17 @@
 Feature: create a new user
-  Scenario: happy user
+  Scenario Outline: happy user
     Given user is on "https://forumet.vimla.se/register"
-    And has entered username "TestLong18UserName"
-    And has entered email "email"
-    And has entered password "password"
-    And has entered confirmed password "confirmedPassword"
+    And has test case <"testCase">
+    And has entered username <"username">
+    And has entered email <"email">
+    And has entered password <"password">
+    And has entered confirmed password <"confirmedPassword">
     When he clicks to registrate
-    Then he gets "TC2:Användarnamnet är för långt"
+    Then he gets <"result">
+
+    Examples:
+    |"testCase"|"username"               |"email"                |"password"|"confirmedPassword"|"result"|
+    |"TC1"     |"TestUserName"           |"mawer30474@jasia.com" |"aaabbbcc"|"aaabbbcc"         |"https://forumet.vimla.se/"|
+    |"TC2"     |"TestLong18UserName"     |"mawer30474@jasia.com" |"aaabbbcc"|"aaabbbcc"         |"Användarnamnet är för långt"|
+    |"TC3"     |"TestUserName"           |"mawer30374@jasia.com" |"aaabbbcc"|"ddaaabbb"         |"Lösenorden är inte likadana. Var god skriv dem på nytt."|
+    |"TC4"     |"TestDoubleUser"         |"mawer30474@jasia.com" |"aaabbbcc"|"aaabbbcc"         |"Denna mailadress finns redan registrerad på ett forumkonto"|
